ecdf543109f1fff518ed3a1fade8d755ca083268
Danila <xgreendaniel@gmail.com>
2022-06-26 19:10:07 +0500

    task29

---
+++ HomeworkNumber4/Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] massiv = new int [8];
+void masmetod (int [] masother){
+    for (int i = 0; i < masother.Length; i++){
+        Console.Write($"Введите элемент №{i+1}: ");
+        masother[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Массив - [");
+    for (int i = 0; i < masother.Length - 1; i++){
+        Console.Write($"{masother[i]}, ");
+    }
+    Console.Write($"{masother[masother.Length - 1]}");
+    Console.Write("]");
+}
+masmetod(massiv);
