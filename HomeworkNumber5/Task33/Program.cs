3d42280770df6aed805fbbf6fba57a175718a1e1
Danila <xgreendaniel@gmail.com>
2022-06-29 12:09:58 +0500

    task33

---
+++ HomeworkNumber5/Task33/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+Console.Write("Введите длину массива: ");
+int maslenght = Convert.ToInt32(Console.ReadLine());
+int [] masran = new int [maslenght];
+masrandom(masran);
+void masrandom (int [] n1){
+    for (int i = 0; i < n1.Length; i++){
+        n1[i]  = new Random().Next(1,11);
+    }
+    Console.Write("Введите число, которое нужно найти в массиве: ");
+    int search = Convert.ToInt32(Console.ReadLine());
+    int searchcount = 0;
+    for (int i = 0; i < n1.Length - 1; i++){
+        if (n1[i] == search){
+            searchcount++;
+        }
+    }
+    if (searchcount >= 1){
+        Console.WriteLine($"Число {search} присутствует в массиве");
+    }
+    else{
+        Console.WriteLine($"Число {search} отсутствует в массиве");
+    }
+    Console.Write("Исходный массив: [");
+    for (int i = 0; i < n1.Length - 1; i++){
+            Console.Write($"{n1[i]}, ");
+    }
+    Console.Write($"{n1[n1.Length - 1]}");
+    Console.Write("]");
+}
