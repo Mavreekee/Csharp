d6af42dcf0b9818269c76060f261469f6f1e4e0a
Danila <xgreendaniel@gmail.com>
2022-07-06 20:30:13 +0500

    task52

---
+++ HomeworkNumber7/Task52/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+Console.WriteLine("Задайте размерность массива m х n");
+Console.Write("Введите m: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [row,column];
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+{
+    matrix[i,j] = new Random().Next(0,10);
+    Console.Write(matrix[i,j] + " " );
+}
+Console.WriteLine();
+}
+double sum = 0;
+int count = 0;
+    for (int j = 0; j < column; j++)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            sum = sum + matrix[i,j];
+            if (i == row - 1)
+            {
+            Console.WriteLine($"Среднее арифметическое {count}-го столбца равно {Math.Round(sum/row,2)}");
+            sum = 0;
+            count = count + 1;
+            }
+        }
+    }
