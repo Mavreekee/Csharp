0b82f4d631149d6d75aa70b0cb32f48d6c7b3095
Danila <xgreendaniel@gmail.com>
2022-07-06 20:06:33 +0500

    task47

---
+++ HomeworkNumber7/Task47/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+Console.WriteLine("Задайте размерность массива m х n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double [m,n];
+Console.WriteLine($"Получившийся массив {m} х {n}:");
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+    matrix[i,j] = Math.Round((new Random().NextDouble())*10, 2);
+    Console.Write(matrix[i,j] + " " );
+}
+Console.WriteLine();
+}
