9f7ec94f6e4b5cda9c93ee1882aa78b0cf314d4b
Danila <xgreendaniel@gmail.com>
2022-07-15 11:11:28 +0500

    task60

---
+++ HomeworkNumber8/Task60/Program.cs
@@ -0,0 +1,29 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+int rows = new Random().Next(1,5);
+int colums = new Random().Next(1,5);
+int faces = new Random().Next(1,5);
+int [] repeats = new int [100];
+int [,,] matrix = new int [rows,colums,faces];
+Console.WriteLine("Исходный трехмерный массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < colums; j++)
+    {
+        for (int k = 0; k < faces; k++)
+        {
+        matrix[i,j,k] = new Random().Next(10,99);
+        repeats[matrix[i,j,k]] = repeats[matrix[i,j,k]] + 1;
+        while (repeats[matrix[i,j,k]] > 1)
+            {
+                matrix[i,j,k] = new Random().Next(10,99);
+
+            }
+        Console.Write($"[{i},{j},{k}] = {matrix[i,j,k]}" + "  ");
+        }
+    Console.WriteLine();
+    }
+    if (i < rows - 1)
+        {
+            Console.WriteLine();
+        }
+}
