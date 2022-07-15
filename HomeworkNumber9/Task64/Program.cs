49c307066635d799719862a6cd66960aec6c7e1d
Danila <xgreendaniel@gmail.com>
2022-07-15 21:50:11 +0500

    hwnumber9

---
+++ HomeworkNumber9/Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Натуральные числа от {N} до 1: ");
+void OutputNumber (int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+Console.Write($"{N} ");
+OutputNumber(N-1);
+}
+OutputNumber(N);
+Console.WriteLine();
---
+++ HomeworkNumber9/Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма натуральных чисел в промежутке от {M} до {N} = ");
+int sumNumber (int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    return M + sumNumber(M + 1, N);
+}
+Console.Write(sumNumber(M,N));
