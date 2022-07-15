dcb7097edec7858889f54edf08c0b5aad6f9b6ee
Danila <xgreendaniel@gmail.com>
2022-07-15 11:10:25 +0500

    task54

---
+++ HomeworkNumber8/Task54/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+int rows = new Random().Next(1,10);
+int colums = new Random().Next(1,10);
+int [,] matrix = new int [rows,colums];
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < colums; j++)
+    {
+        matrix[i,j] = new Random().Next(-10,10);
+        Console.Write(matrix[i,j] + " ");
+    }
+Console.WriteLine();
+}
+Console.WriteLine();
+
+void sortElements (int [,] matrix)
+{
+int x = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < (matrix.GetLength(1)-1); k++)
+        {
+        if (matrix[i,k] < matrix[i,k+1])
+        {
+            x = matrix[i,k+1];
+            matrix[i,k+1] = matrix[i,k];
+            matrix[i,k] = x;
+        }
+        }
+    }
+}
+Console.WriteLine("Массив после сортировки:");
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i,j] + " ");
+    }
+Console.WriteLine();
+}
+}
+sortElements(matrix);
